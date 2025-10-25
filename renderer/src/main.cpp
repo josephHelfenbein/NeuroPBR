1807f3424fb056ab1ff047b555348b612a9c46de
josephHelfenbein <josephhelfenbein@gmail.com>
2025-10-25 03:45:40 -0400

    add cmakelists and empty files for renderer structure

---
+++ renderer/src/main.cpp
@@ -0,0 +1,28 @@
+#pragma once
+
+
+int main() {
+    
+    float planeVertices[] = {
+        -1.0f, 0.0f,  1.0f,
+         1.0f, 0.0f,  1.0f,
+    };
+    float planeNormals[] = {
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+    };
+    float planeTexCoords[] = {
+        0.0f, 1.0f,
+        1.0f, 1.0f,
+    };
+    float planeIndices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+
+
+
+
+    return 0;
+}
