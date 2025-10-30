6b72914d53fc5ba267cfe90ebe80ec00c7eface4
DereckTav <derecktaverne@gmail.com>
2025-10-29 23:21:50 -0400

    cleaned up encoders/unet.py, and adjusted test encoders/unet.py

--- training/models/encoders/unet.py
+++ training/models/encoders/unet.py
@@ -78,7 +78,7 @@ class StrideEncoderBlock(nn.Module):
         return x, skip
 
 class UNetEncoder(nn.Module):
-    def __init__(self, in_channels: int, channel_list: list | None = None, skip: bool =False):
+    def __init__(self, in_channels: int, channel_list: list | None = None, skip: bool = True):
         super().__init__()
 
         if channel_list is None:
@@ -115,7 +115,7 @@ class UNetEncoder(nn.Module):
         return x, skips
 
 class UNetStrideEncoder(nn.Module):
-    def __init__(self, in_channels: int, channel_list: list | None = None, skip: bool =False):
+    def __init__(self, in_channels: int, channel_list: list | None = None, skip: bool = True):
         super().__init__()
 
         if channel_list is None:
@@ -146,23 +146,23 @@ class UNetStrideEncoder(nn.Module):
         return x, skips
 
 # --> Unet encoder with resnet backbone
-"""
+'''
 backbone: 
 'resnet18'
 'resnet34'
 'resnet50'
 'resnet101'
 'resnet152'
-"""
+'''
 class UNetResNetEncoder(nn.Module):
     def __init__(
             self,
             in_channels: int,
             backbone: Literal['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'] ='resnet101',
-            freeze_backbone: bool =False,
-            freeze_bn:bool =False,
+            freeze_backbone: bool = False,
+            freeze_bn:bool = False,
             stride: Literal[1, 2] = 2,
-            skip:bool =True # true because decoder for resnet would be expecting skips
+            skip:bool = True # true because decoder for resnet would be expecting skips
     ):
         super().__init__()
 
